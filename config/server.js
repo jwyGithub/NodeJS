df7c15ae4366af0613eaa5226c2d9260be96270b
jiangweiye <jiangweiye@outlook.com>
2020-03-06 20:58:06 +0800

    12321

--- config/server.js
+++ config/server.js
@@ -1,16 +1,16 @@
 module.exports = {
   local: {
-    open: true,
+    open: false,
     baseUrl: "localhost",
     mongobaseUrl: "34.92.230.15",
     mongoPort: 27017,
     port: 3001
   },
   http: {
-    open: false,
-    baseUrl: "34.92.230.15",
+    open: true,
+    baseUrl: "jwycloud.xyz",
     mongoPort: 27017,
-    port: 8080
+    port: 3001
   },
   https: {
     open: false,
